ac1c80f4db2965a5ce188c4c792395987ee6446e
Bernardo Heynemann <heynemann@gmail.com>
2013-03-25 10:19:02 -0300

    More resilient count

--- cyclops/count.py
+++ cyclops/count.py
@@ -37,22 +37,37 @@ def main(args=None):
     total_request_time = 0
     percentile_request_time = 0
     number_of_servers = 0
+    number_of_servers_with_average = 0
+    number_of_servers_with_percentile = 0
 
     count = 0
     for port in range(last - starting + 1):
         number_of_servers += 1
+
         server_count = get_count(options.host, starting + port)
         print "%s:%d has still %d messages to process" % (options.host, starting + port, server_count['count'])
         count += server_count['count']
-        total_request_time += server_count['average']
-        percentile_request_time += server_count['percentile']
+
+        if server_count['average']:
+            total_request_time += server_count['average']
+            number_of_servers_with_average += 1
+
+        if server_count['percentile']:
+            percentile_request_time += server_count['percentile']
+            number_of_servers_with_percentile += 1
+
+    if number_of_servers_with_average == 0:
+        number_of_servers_with_average = 1
+
+    if number_of_servers_with_percentile == 0:
+        number_of_servers_with_percentile = 1
 
     print
     print "Total of %d messages to send to sentry from the farm at %s." % (count, options.host)
     print
     print "Average sentry response time is %.2fms and 90%% Percentile is %.2fms" % (
-        float(total_request_time) / number_of_servers,
-        float(percentile_request_time) / number_of_servers
+        float(total_request_time) / number_of_servers_with_average,
+        float(percentile_request_time) / number_of_servers_with_percentile
     )
     print
 
