8e8e96bd819ef39c2895fb8bf36e13049ef763bb
Bernardo Heynemann <heynemann@gmail.com>
2013-04-12 19:11:09 -0300

    First test

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+_multiprocess_can_split_ = True
+
+from preggy import *
+
 from cyclops.server import LOGS, ROOT_PATH, DEFAULT_CONFIG_PATH, main
 
 
-def test_server():
-    pass
+def test_server_logs_values():
+    expect(LOGS).to_include(0)
+    expect(LOGS[0]).to_equal("error")
+
+    #LOGS = {
+        #0: 'error',
+        #1: 'warning',
+        #2: 'info',
+        #3: 'debug'
+    #}
+
+
+    #assert True
