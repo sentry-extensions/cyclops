d883fce838d0a474b971c7260ba3f2df142b40d0
Bernardo Heynemann <heynemann@gmail.com>
2013-04-15 09:35:31 -0300

    Couple more server tests

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,9 +3,14 @@
 
 _multiprocess_can_split_ = True
 
-from preggy import *
+from os.path import abspath, join, dirname
+
+from preggy import expect
 
 from cyclops.server import LOGS, ROOT_PATH, DEFAULT_CONFIG_PATH, main
+
+EXPECTED_ROOT_PATH = abspath(join(dirname(__file__), '..'))
+EXPECTED_DEFAULT_CONFIG_PATH = join(ROOT_PATH, 'cyclops', 'local.conf')
 
 
 def test_server_logs_values():
@@ -20,3 +25,8 @@ def test_server_logs_values():
 
     expect(LOGS).to_include(3)
     expect(LOGS[3]).to_equal("debug")
+
+
+def test_paths():
+    expect(ROOT_PATH).to_equal(EXPECTED_ROOT_PATH)
+    expect(DEFAULT_CONFIG_PATH).to_equal(EXPECTED_DEFAULT_CONFIG_PATH)
