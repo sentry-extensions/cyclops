fdda2a51ef9c6ffac65f1d61407f82867cedb8be
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-08-22 18:34:25 +0300

    extract validators

--- cyclops/handlers/router.py
+++ cyclops/handlers/router.py
@@ -23,6 +23,34 @@ class BaseRouterHandler(BaseHandler):
         self.set_status(404)
         self.finish()
 
+    def _403(self):
+        self.set_status(403)
+        self.write("INVALID KEY")
+        self.finish()
+
+    def send_ignored_response(self):
+        self.set_header("X-CYCLOPS-STATUS", "IGNORED")
+        self.application.ignored_items += 1
+        self.write("IGNORED")
+        self.finish()
+
+    def send_processed_response(self):
+        self.set_header("X-CYCLOPS-STATUS", "PROCESSED")
+        self.application.processed_items += 1
+        self.set_status(200)
+        self.write("OK")
+        self.finish()
+
+    def validate_percentage_ignore(self, project_id):
+        is_ignored = False
+        if project_id in self.application.config.IGNORE_PERCENTAGE:
+            value = randint(1, 100)
+            if value < int(self.application.config.IGNORE_PERCENTAGE[project_id]):
+                self.send_ignored_response()
+                is_ignored = True
+
+        return is_ignored
+
     def get_cache_key(self, project_id, request_body):
         try:
             payload = loads(request_body)
@@ -34,7 +62,7 @@ class BaseRouterHandler(BaseHandler):
         return cache_key
 
     def validate_cache(self, project_id, request_body):
-        count = 0
+        is_ignored = False
 
         if self.application.config.URL_CACHE_EXPIRATION > 0:
             cache_key = self.get_cache_key(project_id, request_body)
@@ -46,18 +74,31 @@ class BaseRouterHandler(BaseHandler):
             self.set_header("X-CYCLOPS-CACHE-COUNT", str(count))
 
             if count > self.application.config.MAX_CACHE_USES:
-                self.set_header("X-CYCLOPS-STATUS", "IGNORED")
-                self.application.ignored_items += 1
-                self.write("IGNORED")
-                self.finish()
-            else:
-                self.set_header("X-CYCLOPS-STATUS", "PROCESSED")
+                self.send_ignored_response()
+                is_ignored = True
 
-        return count
+        return is_ignored
+
+    def validate_project_by_public_dsn(self, project_id):
+        is_accessible = True
+
+        if self.application.config.RESTRICT_API_ACCESS:
+            # check project in restrict api list
+            if int(project_id) not in self.application.project_keys:
+                self._404()
+                is_accessible = False
+
+            # get public key
+            sentry_key = self.get_argument('sentry_key').strip()
+
+            # check public key
+            if sentry_key not in self.application.project_keys[project_id]["public_key"]:
+                self._403()
+                is_accessible = False
+
+        return is_accessible
 
     def process_request(self, project_id, url):
-        self.application.processed_items += 1
-
         headers = {}
         body = self.request.body
         for k, v in sorted(self.request.headers.get_all()):
@@ -72,11 +113,7 @@ class BaseRouterHandler(BaseHandler):
         )
 
         self.application.storage.put(project_id, message)
-        #self.application.items_to_process[project_id].put(msgpack.packb(message))
-
-        self.set_status(200)
-        self.write("OK")
-        self.finish()
+        self.send_processed_response()
 
     def handle_backend_post_request(self, project_id=None):
         # check if auth token passed
@@ -113,17 +150,14 @@ class BaseRouterHandler(BaseHandler):
                 self._404()
                 return
 
-        # generate sentry url
-        base_url = self.application.config.SENTRY_BASE_URL.replace('http://', '').replace('https://', '')
-        base_url = "%s://%s:%s@%s" % (self.request.protocol, sentry_key, sentry_secret, base_url)
-        url = "%s%s?%s" % (base_url, self.request.path, self.request.query)
-
         # check cache usage
-        count = self.validate_cache(project_id, self.request.body)
-        if count > self.application.config.MAX_CACHE_USES:
+        if self.validate_cache(project_id, self.request.body):
             return
 
         # handle request
+        base_url = self.application.config.SENTRY_BASE_URL.replace('http://', '').replace('https://', '')
+        base_url = "%s://%s:%s@%s" % (self.request.protocol, sentry_key, sentry_secret, base_url)
+        url = "%s%s?%s" % (base_url, self.request.path, self.request.query)
         self.process_request(project_id, url)
 
     def get_project_id(self, public_key, secret_key):
@@ -148,81 +182,39 @@ class BaseRouterHandler(BaseHandler):
             self.set_header("Access-Control-Allow-Origin", origin)
             self.set_header("Access-Control-Expose-Headers", "X-Sentry-Error, Retry-After")
 
-        # check project in restrict api list
-        if self.application.config.RESTRICT_API_ACCESS:
-            if int(project_id) not in self.application.project_keys:
-                self._404()
-                return
-
-        # check public key
+        # check project accessibility
         project_id = int(project_id)
-        sentry_key = self.get_argument('sentry_key')
-        if self.application.config.RESTRICT_API_ACCESS:
-            if not sentry_key.strip() in self.application.project_keys[project_id]["public_key"]:
-                self.set_status(403)
-                self.write("INVALID KEY")
-                self.finish()
-                return
-
-        # generate sentry url
-        url = "%s%s?%s" % (self.application.config.SENTRY_BASE_URL, self.request.path, self.request.query)
+        if not self.validate_project_by_public_dsn(project_id):
+            return
 
         # ignore by percentage
-        if project_id in self.application.config.IGNORE_PERCENTAGE:
-            value = randint(1, 100)
-
-            if value < int(self.application.config.IGNORE_PERCENTAGE[project_id]):
-                self.set_header("X-CYCLOPS-STATUS", "IGNORED")
-                self.application.ignored_items += 1
-                self.write("IGNORED")
-                self.finish()
-                return
+        if self.validate_percentage_ignore(project_id):
+            return
 
         # check cache usage
-        count = self.validate_cache(project_id, self.request.body)
-        if count > self.application.config.MAX_CACHE_USES:
+        if self.validate_cache(project_id, self.request.body):
             return
 
         # handle request
+        url = "%s%s?%s" % (self.application.config.SENTRY_BASE_URL, self.request.path, self.request.query)
         self.process_request(project_id, url)
 
     def handle_frontend_get_request(self, project_id):
-        # check project in restrict api list
-        if self.application.config.RESTRICT_API_ACCESS:
-            if int(project_id) not in self.application.project_keys:
-                self._404()
-                return
-
-        # check public key
+        # check project accessibility
         project_id = int(project_id)
-        sentry_key = self.get_argument('sentry_key')
-        if self.application.config.RESTRICT_API_ACCESS:
-            if not sentry_key.strip() in self.application.project_keys[project_id]["public_key"]:
-                self.set_status(403)
-                self.write("INVALID KEY")
-                self.finish()
-                return
-
-        # generate sentry url
-        url = "%s%s?%s" % (self.application.config.SENTRY_BASE_URL, self.request.path, self.request.query)
+        if not self.validate_project_by_public_dsn(project_id):
+            return
 
         # ignore by percentage
-        if project_id in self.application.config.IGNORE_PERCENTAGE:
-            value = randint(1, 100)
-
-            if value < int(self.application.config.IGNORE_PERCENTAGE[project_id]):
-                self.set_header("X-CYCLOPS-STATUS", "IGNORED")
-                self.application.ignored_items += 1
-                self.write("IGNORED")
-                self.finish()
-                return
+        if self.validate_percentage_ignore(project_id):
+            return
 
         # check cache usage
-        count = self.validate_cache(project_id, self.get_argument('sentry_data'))
-        if count > self.application.config.MAX_CACHE_USES:
+        if self.validate_cache(project_id, self.get_argument('sentry_data')):
             return
 
         # handle request
+        url = "%s%s?%s" % (self.application.config.SENTRY_BASE_URL, self.request.path, self.request.query)
         self.process_request(project_id, url)
 
 
